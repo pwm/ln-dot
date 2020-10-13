7010dc8fd67ff7d5a4664c00ffd097f3e9712446
zimbatm <zimbatm@zimbatm.com>
2020-10-13 18:55:55 +0200

    nix: only rebuild ln-dot if Haskell source changes
    Avoid unnecessary rebuild

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,12 +1,32 @@
 { pkgs }: with pkgs;
 let
+  src-filter = import ./src-filter.nix;
+
+  # A view of the project's source code, with only the Haskell dependencies
+  ln-dot-src = src-filter.filter {
+    path = ../.;
+    allow = [
+      (src-filter.matchExt "hs")
+      ../app
+      ../ln-dot.cabal
+      ../package.yaml
+      ../src
+      ../src/LnDot
+    ];
+  };
+
   haskellPackages = haskell.packages.ghc883.override {
     overrides =
       let
         generated = haskell.lib.packagesFromDirectory {
           directory = ./packages;
         };
-        manual = haskellPackagesNew: haskellPackagesOld: { };
+        manual = haskellPackagesNew: haskellPackagesOld: {
+          # Inject the filtered source into the package
+          ln-dot = haskellPackagesOld.ln-dot.overrideAttrs (attrs: {
+            src = ln-dot-src;
+          });
+        };
       in
       lib.composeExtensions generated manual;
   };
