097f3e147b2e972d17cdfc345727973ad25b1aa0
zimbatm <zimbatm@zimbatm.com>
2020-10-13 17:02:05 +0000

    A bunch of nix improvements (#1)
    -  only rebuild ln-dot if Haskell source changes
    -  expose all the derivations in the release
    - add hlint derivation

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,12 +1,32 @@
 { pkgs }: with pkgs;
 let
+  src-filter = import ./src-filter.nix;
+
+  # A view of the project's source code, with only the Haskell dependencies
+  ln-dot-src = src-filter.filter {
+    path = ../.;
+    allow = [
+      (src-filter.matchExt "hs")
+      ../app
+      ../ln-dot.cabal
+      ../package.yaml
+      ../src
+      ../src/LnDot
+    ];
+  };
+
   haskellPackages = haskell.packages.ghc883.override {
     overrides =
       let
         generated = haskell.lib.packagesFromDirectory {
           directory = ./packages;
         };
-        manual = haskellPackagesNew: haskellPackagesOld: { };
+        manual = haskellPackagesNew: haskellPackagesOld: {
+          # Inject the filtered source into the package
+          ln-dot = haskellPackagesOld.ln-dot.overrideAttrs (attrs: {
+            src = ln-dot-src;
+          });
+        };
       in
       lib.composeExtensions generated manual;
   };
@@ -17,6 +37,11 @@ let
 in
 {
   exe = haskellPackages.ln-dot;
+
+  # Check the source code with hlint
+  hlint = pkgs.runCommand "hlint" {} ''
+    ${pkgs.hlint}/bin/hlint ${ln-dot-src} | tee $out
+  '';
 
   devEnv = buildEnv {
     name = "ln-dot-env";
